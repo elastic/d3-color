0d38f5b6caa7fbbfab9fc406622256880cc4915b
Elena Shostak <elena.shostak@elastic.co>
2024-06-07 16:08:43 +0200

    Avoid backtracking in v1

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import {terser} from "rollup-plugin-terser";
 import * as meta from "./package.json";
 
+const [pkgNamespace, pkgName] = meta.name.split("/");
+
 const config = {
   input: "src/index.js",
   external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
   output: {
-    file: `dist/${meta.name}.js`,
+    file: `dist/${pkgName}.js`,
     name: "d3",
     format: "umd",
     indent: false,
@@ -22,7 +24,7 @@ export default [
     ...config,
     output: {
       ...config.output,
-      file: `dist/${meta.name}.min.js`
+      file: `dist/${pkgName}.min.js`
     },
     plugins: [
       ...config.plugins,
